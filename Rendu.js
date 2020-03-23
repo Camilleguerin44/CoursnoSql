9011065ac4427415ac18550af93431ddabca3e2b
Camilleguerin44 <35221631+Camilleguerin44@users.noreply.github.com>
2020-03-23 11:20:08 +0100

    Update Rendu.js

--- Rendu.js
+++ Rendu.js
@@ -8,6 +8,12 @@ db.createCollection("sports");
 db.sports.insert([{ name: "foot", nb_player: 11, object_used: "ballon" }, { name: "basket", nb_player: 5, object_used: "ballon de basket" }]);
 
 // Ajouter deux nouveaux field "title" et "require_teams" avec apsert
-db.sports.insert({name:"foot"}, {title:"foot",require_teams:false}, {upsert:true})
+db.sports.update({name:"basket"}, {title:"basket",require_teams:false}, {upsert:true})
+db.sports.update({name:"foot"}, {title:"foot",require_teams:true}, {upsert:true})
 
-//Ajouter un nouveau field pour ajouter le minimum de joueur
+// Ajouter un field "minimum_player" aux sports qui ont besoin d'une équipe
+db.sports.update({require_teams:true}, {minimum_player:5},{upsert:true})
+
+//Ajouter aux documents un nombre de joueur de 10 aux équipes qui auront besoin de joeuurs
+
+db.sports.update( {require_teams:false}, {$inc: {minimum_player:10} )
